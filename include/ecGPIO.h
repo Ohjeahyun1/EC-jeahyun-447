30f0dbd7acd783bfa6e4118eaf681ff574b11ac4
Ohjeahyun <ohyen7@naver.com>
2022-10-01 20:52:54 +0900

    Update ecGPIO.h

--- include/ecGPIO.h
+++ include/ecGPIO.h
@@ -1,8 +1,8 @@
 /*----------------------------------------------------------------\
 @ Embedded Controller by Young-Keun Kim - Handong Global University
-Author           : SSS LAB
+Author           : Oh jeahyun
 Created          : 05-03-2021
-Modified         : 09-20-2022
+Modified         : 10-01-2022
 Language/ver     : C++ in Keil uVision
 
 Description      : Distributed to Students for LAB_GPIO
